8f9f6edcbfa7537c16486a3466d02f8ce34174ab
john Doe <jeffreyalmonte@pursuits-macbook-pro-106.local>
2018-11-16 15:12:07 -0500

    almost done.

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -8,59 +8,76 @@
 
 import UIKit
 
+
+var blankBox = "-"
+var x = "x"
+var o = "o"
+
+//struct TicTacToeMove {
+//    var x: String
+//    var o: String
+//}
 class TicTacToeBrain {
     
+    
+    var playerOne = false
+    var playerTwo = false
+    var gameBoard = [
+        [blankBox,blankBox,blankBox],
+        [blankBox,blankBox,blankBox],
+        [blankBox,blankBox,blankBox]
+                        ]
+    
+    var turnTracker = 0
+    var playerOneWins = false
+    var playerTwoWins = false
+    func enteredMove(){
+        turnTracker += 1
+        if turnTracker % 2 != 0 {
+            playerOne = true
+            playerTwo = false
+        } else {
+            playerOne = false
+            playerTwo = true
+        }
 }
-    
-    
 
-//  THIS IS WHERE YOU WILL MODEL YOUR LOGIC!!!!
-
-
-//this class handles did i win, keep cheking if i win, who's turn it is
-
-
-//let winCombos = [
-//     [(0,0), (0,1), (0,2)],
-//     [(1,0), (1,1), (1,2)],
-//     [(2,0), (2,1), (2,2)],
-//     [(0,0), (1,0), (2,0)],
-//     [(0,1), (1,1), (2,1)],
-//     [(0,2), (1,2), (2,2)],
-//     [(0,0), (1,1), (2,2)],
-//     [(2,0), (1,1), (0,2)]
-//            ]
-let winCombos = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7]
-]
-
-
-var gameState = [0,0,0,0,0,0,0,0,0]
-
-var activePlayer = 1
-
-func squarePressed(_ sender: GameButton) {
-    if activePlayer == 1 {
-        sender.setImage(UIImage(named: "o"), for: .normal)
-        activePlayer = 2
-        
-        
-    } else {
-        sender.setImage(UIImage(named: "x"), for: .normal)
-        activePlayer = 1
+    func gameResult() {
+        for row in gameBoard { // this for loop checks only for horizontal wins
+            if row == [x,x,x] {
+                playerTwoWins = true
+                break
+            } else if row == [o,o,o] {
+                playerOneWins = true
+                break
+            }
+        }
+        for columnIndex in 0..<gameBoard[0].count {
+            var column = [String]()
+            for rowIndex in 0..<gameBoard.count {
+                column.append(gameBoard[rowIndex][columnIndex])
+                if column == [o,o,o] {
+                    playerOneWins = true
+                } else if column == [x,x,x] {
+                    playerTwoWins = true
+                }
+                
+            }
+        }
+        var diagonal1 = [String]()
+        for i in 0..<gameBoard.count {
+            diagonal1.append(gameBoard[i][i])
+        if diagonal1 == [o,o,o] {
+            playerOneWins = true
+        } else if diagonal1 == [x,x,x]{
+            playerTwoWins = true
+            }
     }
-    sender.isUserInteractionEnabled = false
-//
-//for combo in winCombos {
-//    if gameState[[0][0] != (0,0) {
-//        
-//    }
-}
+        var diagonal2 = [String]()
+        for i in 0..<gameBoard.count {
+            diagonal2.append(gameBoard[i][gameBoard.count - 1 - i])
+            }
+
+        }
+    }
 
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,32 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-    // 1 == o, 2 == x
-var activePlayer = 1
-  
     
-
+     let brain = TicTacToeBrain()
+    @IBOutlet var buttons: [GameButton]!
     
+    @IBOutlet var allTheButtons: [GameButton]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
     
-    @IBAction func squarePressed(_ sender: GameButton) {
-        if activePlayer == 1 {
-            sender.setImage(UIImage(named: "o"), for: .normal)
-            activePlayer = 2
+    @IBAction func buttonPressed(_ sender: GameButton) {
+        brain.enteredMove()
+        if brain.playerOne {
+            brain.gameBoard[sender.row][sender.col] = o
+                sender.setBackgroundImage(UIImage(named: "o"), for: .normal)
             
-        } else {
-            sender.setImage(UIImage(named: "x"), for: .normal)
-            activePlayer = 1
+
+            brain.gameResult()
+            if brain.playerOneWins {
+                print("player One Wins!")
+                allTheButtons.forEach{(button) in
+                    button.isUserInteractionEnabled = false
+                    
+                }
+            }
+        
+        } else if brain.playerTwo {
+            brain.gameBoard[sender.row][sender.col] = x
+            sender.setBackgroundImage(UIImage(named: "x"), for: .normal)
+            brain.gameResult()
+            if brain.playerTwoWins {
+                print("player Two Wins!")
+                allTheButtons.forEach{(button) in
+                    button.isUserInteractionEnabled = false
+                    
+                }
+            }
+            
         }
-        sender.isUserInteractionEnabled = false
 
     }
+    
 
-    override func viewDidLoad() {
-    super.viewDidLoad()
-        
-      
-  }
-
+   
+//    @IBAction func resetGame(_ resetGameButton: gameButton) {
+//        cards = cards.shuffled()
+//        for (index, cardButton) in allCardButtons.enumerated() {
+//            cards[index].tag = index
+//            cardButton.tag = index
+//            cardButton.isEnabled = true
+//            cardButton.setImage(UIImage(named: MatchingGameName.pokemonCardBack), for: .normal)
+//        }
+//    }
 
 }
-
